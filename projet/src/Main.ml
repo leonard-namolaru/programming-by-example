7741e89dc41bd217ca5c76d16f7ac91b16ed4e0e
namolaru <leonard.namolaru@yahoo.com>
2022-04-05 13:46:33 +0200

    verification que les chemins qui ont été passés représentent un fichier

--- projet/src/Main.ml
+++ projet/src/Main.ml
@@ -10,6 +10,18 @@
 let _ = print_string "Array.length : " ; print_int (Array.length Sys.argv) ; print_newline ()
 let _ = Array.iter (fun x -> print_string x ; print_newline () ) Sys.argv
 
+(* Vérifier que les chemins qui nous ont été passés représentent bien un fichier *)
+let rec verification_chemins chemins_liste = match chemins_liste with
+                                                 |[] -> true
+                                                 |h::t -> if Sys.file_exists h then
+                                                             if not (Sys.is_directory h) then
+                                                                verification_chemins t
+                                                             else
+                                                                false
+                                                          else
+                                                                false
+let argv_list = Array.to_list Sys.argv
+let _ = if (verification_chemins (List.tl argv_list)) then print_string "paths ok !" else print_string "paths pbm !" ; print_newline ()
 
 
 

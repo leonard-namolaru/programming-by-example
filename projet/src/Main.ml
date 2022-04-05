169d3e046cde223a96e0a67854dd0a0cf7047335
namolaru <leonard.namolaru@yahoo.com>
2022-04-05 22:04:40 +0200

    réorganisation du code

--- projet/src/Main.ml
+++ projet/src/Main.ml
@@ -6,24 +6,21 @@
  *
  *)
 
-(* Test : Imprimer tous les paramètres passés via la ligne de commande *)
-let _ = print_string "Array.length : " ; print_int (Array.length Sys.argv) ; print_newline ()
-let _ = Array.iter (fun x -> print_string x ; print_newline () ) Sys.argv
+(* Nombre de paramètres passés via la ligne de commande *)
+let argc = Array.length Sys.argv
 
-(* Vérifier si le nombre de paramètres passés via la ligne de commande est correct *)
-let _ = let argc = Array.length Sys.argv in
-               if ((argc < 2) || (argc > 3)) then
-                  begin
-                  print_string "Exemple d'utilisation : ./Main.exe ../exemples/etape1/date_to_month [../exemples/etape1/date_verbose]";
-                  print_newline ();
-                  exit 0;
-                  end
-               else
-                   ()
+(* Verification si le nombre de paramètres passés via la ligne de commande est correct *)
+let _ =  if ((argc < 2) || (argc > 3)) then
+            begin
+               Printf.printf "Utilisation : %s <fichier> [<fichier>] \n%!" Sys.argv.(0) ;
+               Printf.printf "par exemple : %s ../exemples/etape1/date_to_month \n%!" Sys.argv.(0) ;
+               exit 0;
+            end
+         else ()
 
-(* Vérifier que les chemins qui nous ont été passés représentent bien un fichier *)
 
-let rec verification_chemins chemins_liste = match chemins_liste with
+(* Verification si les chemins qui nous ont été passés représentent bien un fichier *)
+let rec verification_chemins liste_chemins = match liste_chemins with
                                                  |[] -> true
                                                  |h::t -> if Sys.file_exists h then
                                                              if not (Sys.is_directory h) then
@@ -31,16 +28,35 @@ let rec verification_chemins chemins_liste = match chemins_liste with
                                                              else
                                                                 false
                                                           else
-                                                                false
-let argv_list = Array.to_list Sys.argv
-let _ = if (verification_chemins (List.tl argv_list)) then print_string "paths ok !" else print_string "paths pbm !" ; print_newline ()
+                                                            false
 
-let file_to_list fichier = let rec f liste input_channel = match input_line input_channel with
+(* On n'a pas besoin du premier élément de la liste car ce n'est pas un des paramètres 
+que l'utilisateur a choisi de transférer au logiciel mais le nom du fichier exécutable *)
+let liste_fichiers = List.tl (Array.to_list Sys.argv)
+
+let _ = if not (verification_chemins liste_fichiers) then 
+            begin
+               print_endline "Le système a détecté qu'un chemin qui lui est transféré ne mène pas à un fichier valide.";
+               exit 0; 
+            end
+        else ()
+
+let input_file_to_list fichier = 
+   let rec f liste input_channel = match input_line input_channel with
                                                 | next_line -> f (liste@[next_line]) input_channel
                                                 | exception End_of_file -> close_in input_channel ; liste
-in f [] (open_in fichier)
+   in f [] (open_in fichier)
 
+let input_output_file_to_lists fichier = 
+   let rec f input_list output_list input_channel = match input_line input_channel with
+                                                | next_line -> let tmp_list = String.split_on_char '\t' next_line in
+                                                                  f (input_list@[List.nth tmp_list 0]) (output_list@[List.nth tmp_list 1]) input_channel
+                                                | exception End_of_file -> close_in input_channel ; (input_list, output_list)
+in f [] [] (open_in fichier)
 
+let input_output_lists = input_output_file_to_lists (List.nth liste_fichiers 0)
+let input_list = fst input_output_lists 
+let output_list = snd input_output_lists 
 
-let my_liste = file_to_list (List.nth (List.tl argv_list) 0)
-let _ = print_string (List.nth my_liste 0) ; print_newline ()
+let _ = print_endline (List.nth input_list 0) ; print_endline (List.nth output_list 0)
+let _ = print_endline (List.nth input_list 1) ; print_endline (List.nth output_list 1)

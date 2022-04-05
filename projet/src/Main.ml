aed8bcf5496974710b5678211ef9aa3e8fea39d1
namolaru <leonard.namolaru@yahoo.com>
2022-04-05 14:08:18 +0200

    Vérifier si le nombre de paramètres passés est correct

--- projet/src/Main.ml
+++ projet/src/Main.ml
@@ -10,7 +10,19 @@
 let _ = print_string "Array.length : " ; print_int (Array.length Sys.argv) ; print_newline ()
 let _ = Array.iter (fun x -> print_string x ; print_newline () ) Sys.argv
 
+(* Vérifier si le nombre de paramètres passés via la ligne de commande est correct *)
+let _ = let argc = Array.length Sys.argv in
+               if ((argc < 2) || (argc > 3)) then
+                  begin
+                  print_string "Exemple d'utilisation : ./Main.exe ../exemples/etape1/date_to_month [../exemples/etape1/date_verbose]";
+                  print_newline ();
+                  exit 0;
+                  end
+               else
+                   ()
+
 (* Vérifier que les chemins qui nous ont été passés représentent bien un fichier *)
+
 let rec verification_chemins chemins_liste = match chemins_liste with
                                                  |[] -> true
                                                  |h::t -> if Sys.file_exists h then

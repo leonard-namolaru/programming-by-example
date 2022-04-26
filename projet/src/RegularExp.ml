650fcc09795ea662e8b874cbd72234fe4ae1fb5c
namolaru <leonard.namolaru@yahoo.com>
2022-04-26 16:16:43 +0200

    dev en cours : Connaitre l’ensemble des expressions régulières qui filtrent str

--- projet/src/RegularExp.ml
+++ projet/src/RegularExp.ml
@@ -233,3 +233,54 @@ let rendre_la_monnaie (somme:int) (nb_type_de_pieces:int) (valeurs_de_pieces:int
 let _ = rendre_la_monnaie 8 3 [1;4;6]
 
 
+(* ---------------------------------------------------------------------------- *)
+
+(* Connaitre l’ensemble des expressions régulières qui filtrent str *) 
+(* Calcule de cette information de façon ascendante (bottom-up) en s’inspirant de la programmation dynamique *)
+
+type reg = |Tokens of (token list) |Infinity |None
+
+let get_reg_len reg = match reg with |Tokens token_list -> List.length token_list
+                                     |Infinity -> Int.max_int 
+                                     |None -> 0 
+
+let concat_reg reg1 reg2 = match reg1, reg2 with
+                     |Tokens token_list1,_ -> Tokens (token_list1@[])
+                     |_ -> None
+
+let ensemble_expressions_regulieres_filtrent_str (str:string) (nb_type_de_tokens:int) (tokens:token list) =
+  (* matrice[i, 0] = 0 ∀ i *) (* Une expression régulière qui filtre les 0 premiers char de str ? il me faut combien de tokens ? 0, donc une liste vide *)
+  let rec cas_simple_1 index nb_type_de_tokens matrice  = match (index = (nb_type_de_tokens + 1))  with 
+                                           |true -> matrice
+                                           |false -> cas_simple_1 (index + 1) nb_type_de_tokens (matrice@[[None]])
+
+  (* matrice[0, s] = {0 si s = 0 , ∞ sinon *) 
+  (* Si je n’ai pas de tokens et on me demande de construire l’expression régulière qui filtre les s premiers char de str ? : je ne peux pas, (sauf si s est 0). *)
+  in let rec cas_simple_2 s longeur_str matrice = match (s = longeur_str), matrice  with 
+                        |false,(h::t) -> cas_simple_2 (s + 1) longeur_str ([(h@[Infinity])]@t) 
+                        |_,_-> matrice
+
+  in let rec cas_general s i nb_type_de_tokens longeur_str matrice tokens = match (i = (nb_type_de_tokens + 1))  with
+    |true -> matrice
+    |false -> match (s = (longeur_str + 1) ) with
+              |true -> cas_general 1 (i + 1) nb_type_de_tokens longeur_str matrice tokens
+              |false -> 
+                let filtrage = filtrage_mot_par_token (String.sub str 0 longeur_str) (List.nth tokens (i-1)) in 
+                  if filtrage then 
+                    begin
+                    let min_option1 = get_reg_len (list_list_get matrice (i-1) s) in 
+                    let min_option2 = 1 + get_reg_len (list_list_get matrice i (s -1)) in
+                    if min_option2 != min_option1
+                      then cas_general (s+1) i nb_type_de_tokens longeur_str (list_list_ajout_element_position_ligne_colonne matrice (concat_reg (Tokens [(List.nth tokens (i-1))]) (list_list_get matrice i (s -1)) ) i s) tokens
+                    else
+                      cas_general (s+1) i nb_type_de_tokens longeur_str (list_list_ajout_element_position_ligne_colonne matrice (list_list_get matrice (i-1) s) i s) tokens
+                    end
+                  else 
+                    cas_general (s+1) i nb_type_de_tokens longeur_str (list_list_ajout_element_position_ligne_colonne matrice (list_list_get matrice (i-1) s) i s) tokens
+
+  in cas_general 1 1 nb_type_de_tokens (String.length str) (cas_simple_2 0 (String.length str) (cas_simple_1 0 nb_type_de_tokens [])) tokens
+
+let _ = ensemble_expressions_regulieres_filtrent_str "stlen" 4 [Plus Lower; Plus Upper; Plus Alphanumeric ; Plus Alpha]
+
+
+

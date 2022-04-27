c56a3e9a92150c935d85a7e851c6a7bb2868e710
namolaru <leonard.namolaru@yahoo.com>
2022-04-27 11:58:11 +0200

    ensemble des expressions régulières qui filtrent un string

--- projet/src/RegularExp.ml
+++ projet/src/RegularExp.ml
@@ -195,92 +195,40 @@ let _ = list_list_ajout_element_position_ligne_colonne [[1;3];[4;5;6]] 2 0 1
 let _ = list_ajout_element_position_i [Infinity;Infinity] (Entier 3) 1 
 let _ = list_list_get (list_list_ajout_element_position_ligne_colonne [[1;3];[4;5;6]] 2 0 1) 0 1
 
-(* TEST : Traduction d'un algo prog dynamique "Rendre la monnaie" en Ocaml *)
-type entier = |Entier of int |Infinity
-
-let get_entier entier = match entier with |Entier x -> x |Infinity -> Int.max_int 
-let set_entier entier = Entier entier
-
-let rendre_la_monnaie (somme:int) (nb_type_de_pieces:int) (valeurs_de_pieces:int list) =
-  (* matrice[i, 0] = 0 ∀ i *) (* Faire la somme 0 avec les pièces de valeurs v1 ... vi ? il me faut combien de pièces ? 0 *)
-  let rec cas_simple_1 index nb_type_de_pieces matrice  = match (index = (nb_type_de_pieces + 1))  with 
-                                           |true -> matrice
-                                           |false -> cas_simple_1 (index + 1) nb_type_de_pieces (matrice@[[set_entier 0]])
-
-  (* matrice[0, s] = {0 si s = 0 , ∞ sinon *) (* Si je n’ai pas de pièces et on me demande de faire la somme S : je ne peux pas, (sauf si la somme est 0). *)
-  in let rec cas_simple_2 s somme matrice = match (s = somme), matrice  with 
-                        |false,(h::t) -> cas_simple_2 (s + 1) somme ([(h@[Infinity])]@t) 
-                        |_,_-> matrice
-
-  in let rec cas_general s i nb_type_de_pieces somme matrice valeurs_de_pieces = match (i = (nb_type_de_pieces + 1))  with
-    |true -> matrice
-    |false -> match (s = (somme + 1) ) with
-              |true -> cas_general 1 (i + 1) nb_type_de_pieces somme matrice valeurs_de_pieces
-              |false -> 
-                let valeur_piece_i = List.nth valeurs_de_pieces (i-1) in 
-                  if (s >= valeur_piece_i) then 
-                    begin
-                    let min_option1 = get_entier (list_list_get matrice (i-1) s) in 
-                    let min_option2 = 1 + get_entier (list_list_get matrice i (s - valeur_piece_i)) in
-                    let minimum = set_entier (min min_option1 min_option2) in
-                    cas_general (s+1) i nb_type_de_pieces somme (list_list_ajout_element_position_ligne_colonne matrice minimum i s) valeurs_de_pieces
-                    end
-                  else 
-                    cas_general (s+1) i nb_type_de_pieces somme (list_list_ajout_element_position_ligne_colonne matrice (list_list_get matrice (i-1) s) i s) valeurs_de_pieces
-
-  in cas_general 1 1 nb_type_de_pieces somme (cas_simple_2 0 somme (cas_simple_1 0 nb_type_de_pieces [])) valeurs_de_pieces
-
-let _ = rendre_la_monnaie 8 3 [1;4;6]
-
-
 (* ---------------------------------------------------------------------------- *)
 
 (* Connaitre l’ensemble des expressions régulières qui filtrent str *) 
 (* Calcule de cette information de façon ascendante (bottom-up) en s’inspirant de la programmation dynamique *)
 
-type reg = |Tokens of (token list) |Infinity |None
-
-let get_reg_len reg = match reg with |Tokens token_list -> List.length token_list
-                                     |Infinity -> Int.max_int 
-                                     |None -> 0 
-
-let concat_reg reg1 reg2 = match reg1, reg2 with
-                     |Tokens token_list1,_ -> Tokens (token_list1@[])
-                     |_ -> None
-
-let ensemble_expressions_regulieres_filtrent_str (str:string) (nb_type_de_tokens:int) (tokens:token list) =
-  (* matrice[i, 0] = 0 ∀ i *) (* Une expression régulière qui filtre les 0 premiers char de str ? il me faut combien de tokens ? 0, donc une liste vide *)
-  let rec cas_simple_1 index nb_type_de_tokens matrice  = match (index = (nb_type_de_tokens + 1))  with 
-                                           |true -> matrice
-                                           |false -> cas_simple_1 (index + 1) nb_type_de_tokens (matrice@[[None]])
-
-  (* matrice[0, s] = {0 si s = 0 , ∞ sinon *) 
-  (* Si je n’ai pas de tokens et on me demande de construire l’expression régulière qui filtre les s premiers char de str ? : je ne peux pas, (sauf si s est 0). *)
-  in let rec cas_simple_2 s longeur_str matrice = match (s = longeur_str), matrice  with 
-                        |false,(h::t) -> cas_simple_2 (s + 1) longeur_str ([(h@[Infinity])]@t) 
-                        |_,_-> matrice
-
-  in let rec cas_general s i nb_type_de_tokens longeur_str matrice tokens = match (i = (nb_type_de_tokens + 1))  with
+let ensemble_expressions_regulieres_filtrent_str (str:string) (tokens:token list) =
+  let rec construction_matrice index_str index_token nb_type_de_tokens longeur_str matrice tokens = match (index_token = nb_type_de_tokens)  with
     |true -> matrice
-    |false -> match (s = (longeur_str + 1) ) with
-              |true -> cas_general 1 (i + 1) nb_type_de_tokens longeur_str matrice tokens
+    |false -> match (index_str = longeur_str) with
+              |true -> construction_matrice 0 (index_token + 1) nb_type_de_tokens longeur_str matrice tokens
               |false -> 
-                let filtrage = filtrage_mot_par_token (String.sub str 0 longeur_str) (List.nth tokens (i-1)) in 
-                  if filtrage then 
-                    begin
-                    let min_option1 = get_reg_len (list_list_get matrice (i-1) s) in 
-                    let min_option2 = 1 + get_reg_len (list_list_get matrice i (s -1)) in
-                    if min_option2 != min_option1
-                      then cas_general (s+1) i nb_type_de_tokens longeur_str (list_list_ajout_element_position_ligne_colonne matrice (concat_reg (Tokens [(List.nth tokens (i-1))]) (list_list_get matrice i (s -1)) ) i s) tokens
-                    else
-                      cas_general (s+1) i nb_type_de_tokens longeur_str (list_list_ajout_element_position_ligne_colonne matrice (list_list_get matrice (i-1) s) i s) tokens
-                    end
-                  else 
-                    cas_general (s+1) i nb_type_de_tokens longeur_str (list_list_ajout_element_position_ligne_colonne matrice (list_list_get matrice (i-1) s) i s) tokens
+                let filtrage_resultat = 
+                  if (filtrage_mot_par_token (String.sub str index_str 1) (List.nth tokens index_token)) 
+                    then Option.some (List.nth tokens index_token)
+                  else Option.none
+                in construction_matrice (index_str + 1) index_token nb_type_de_tokens longeur_str (list_list_ajout_element_position_ligne_colonne matrice filtrage_resultat index_token index_str) tokens   
+  
+  
+  construction_matrice 0 0 (List.length tokens) (String.length str) [] tokens 
 
-  in cas_general 1 1 nb_type_de_tokens (String.length str) (cas_simple_2 0 (String.length str) (cas_simple_1 0 nb_type_de_tokens [])) tokens
-
-let _ = ensemble_expressions_regulieres_filtrent_str "stlen" 4 [Plus Lower; Plus Upper; Plus Alphanumeric ; Plus Alpha]
+let _ = ensemble_expressions_regulieres_filtrent_str "ab8" [Plus Lower; Plus Alpha; Plus Alphanumeric ; Plus Numeric]
 
 
+(*
+   
+  in let rec construction_resultat index_str index_token nb_type_de_tokens longeur_str matrice tokens ensemble_expressions = match (index_token = nb_type_de_tokens)  with
+  |true -> ensemble_expressions
+  |false -> match (index_str = longeur_str) with
+            |true -> construction_resultat 0 (index_token + 1) nb_type_de_tokens longeur_str matrice tokens
+            |false -> 
+              let element = list_list_get liste ligne colonne
+                if (filtrage_mot_par_token (String.sub str index_str 1) (List.nth tokens index_token)) 
+                  then Option.some (List.nth tokens index_token)
+                else Option.none
+              in construction_resultat (index_str + 1) index_token nb_type_de_tokens longeur_str (list_list_ajout_element_position_ligne_colonne matrice filtrage_resultat index_token index_str) tokens 
 
+*)
